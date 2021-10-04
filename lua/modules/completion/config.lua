5b6fcbde1f8cd547e4b2d0101b9f2828bb3bb759
zrecovery <zrecovery@outlook.com>
2021-10-04 09:06:13 +0800

    移除无用函数

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -120,19 +120,6 @@ end
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-    return false
-  end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local feedkey = function(key)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), "n", true)
-end
-
-
 function config.cmp()
   local cmp = require "cmp"
   local luasnip = require "luasnip"
