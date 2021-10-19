0fa21560b02f092a761206a6847ff8388b3e56b4
zrecovery <zrecovery@outlook.com>
2021-10-19 20:30:00 +0800

    [功能]添加对Python,JSON,CSS,C/C++的LSP服务支持

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -62,7 +62,7 @@ function config.lspcfg()
   }
   
     -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-    local servers = { 'sourcekit'}
+    local servers = { 'sourcekit','pyright', "jsonls", 'cssls','clangd'}
     for _, lsp in ipairs(servers) do
       require('lspconfig')[lsp].setup {
         capabilities = capabilities,
@@ -120,9 +120,16 @@ end
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
+
 function config.cmp()
-  local cmp = require "cmp"
-  local luasnip = require "luasnip"
+  local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  end
+  
+  local luasnip = require("luasnip")
+  local cmp = require("cmp")
+  
   cmp.setup {
     snippet = {
       expand = function(args)
@@ -136,10 +143,8 @@ function config.cmp()
         -- set a name for each source
         vim_item.menu =
           ({
-          buffer = "[Buffer]",
           nvim_lsp = "[LSP]",
           nvim_lua = "[Lua]",
-          cmp_tabnine = "[TabNine]",
           path = "[Path]",
           luasnip = "[LuaSnip]"
         })[entry.source.name]
@@ -147,30 +152,34 @@ function config.cmp()
       end
     },
     mapping = {
-      ['<Tab>'] = function(fallback)
+      ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
-    end,
-    ['<S-Tab>'] = function(fallback)
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+        luasnip.jump(-1)
       else
         fallback()
       end
-    end,
-          },
+    end, { "i", "s" }),
+
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+         },
     sources = {
       {name = "buffer"},
       {name = "nvim_lsp"},
       {name = "path"},
-      {name = "cmp_tabnine"},
       {name = "nvim_lua"},
       {name = "luasnip"}
     },
