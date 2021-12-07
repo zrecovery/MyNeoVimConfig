12080640041edc7a4193b0894d0f472f7085fd7f
zrecovery <zrecovery@outlook.com>
2021-12-08 07:23:41 +0800

    [功能]将Lua语言服务修改为默认配置

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -19,50 +19,8 @@ function config.lspcfg()
     },
   }
 
-  -- Lua Language Server
-  local system_name
-  if vim.fn.has("mac") then
-     system_name = "macOS"
-  else
-     print("Unsupported system for sumneko")
-  end
-   
-  local home= os.getenv("HOME")
-  local sumneko_root_path = home .. '/Project/lua-language-server'
-  local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
- 
-  local runtime_path = vim.split(package.path, ';')
-  table.insert(runtime_path, "lua/?.lua")
-  table.insert(runtime_path, "lua/?/init.lua")
-  
-  require('lspconfig').sumneko_lua.setup {
-    cmd = {sumneko_binary, "-E", sumneko_root_path.. "/main.lua"};
-    settings = {
-      Lua = {
-        runtime = {
-          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-          version = 'LuaJIT',
-          -- Setup your lua path
-          path = runtime_path,
-        },
-        diagnostics = {
-          -- Get the language server to recognize the `vim` global
-          globals = {'vim'},
-        },
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = vim.api.nvim_get_runtime_file("", true),
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-          enable = false,
-        },
-      },
-    },
-  }
-  
     -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-    local servers = { 'sourcekit','pyright',"yamlls", "jsonls", 'cssls','clangd'}
+    local servers = { 'sourcekit','pyright',"yamlls", "jsonls", 'cssls','clangd', 'sumneko_lua'}
     for _, lsp in ipairs(servers) do
       require('lspconfig')[lsp].setup {
         capabilities = capabilities,
