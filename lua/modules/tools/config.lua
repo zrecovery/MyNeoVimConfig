54f419835f8302de28d5bcd65fc4c099a89ada34
zrecovery <zrecovery@outlook.com>
2022-02-26 19:00:20 +0800

    [功能]移除无用配置

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -1,33 +1,5 @@
 local config = {}
 
-local function load_env_file()
-  local env_file = os.getenv("HOME")..'/.env'
-  local env_contents = {}
-  if vim.fn.filereadable(env_file) ~= 1 then
-    print('.env file does not exist')
-    return
-  end
-  local contents = vim.fn.readfile(env_file)
-  for _,item in pairs(contents) do
-    local line_content = vim.fn.split(item,"=")
-    env_contents[line_content[1]] = line_content[2]
-  end
-  return env_contents
-end
-
-
-function config.vim_dadbod_ui()
-  if packer_plugins['vim-dadbod'] and not packer_plugins['vim-dadbod'].loaded then
-    vim.cmd [[packadd vim-dadbod]]
-  end
-  vim.g.db_ui_show_help = 0
-  vim.g.db_ui_win_position = 'left'
-  vim.g.db_ui_use_nerd_fonts = 1
-  vim.g.db_ui_winwidth = 35
-  vim.g.db_ui_save_location = os.getenv("HOME") .. '/.cache/vim/db_ui_queries'
-  vim.g.dbs = load_dbs()
-end
-
 function config.vim_vista()
   vim.g['vista#renderer#enable_icon'] = 1
   vim.g.vista_disable_statusline = 1
--- lua/modules/tools/plugins.lua
+++ lua/modules/tools/plugins.lua
@@ -1,27 +1,13 @@
 local tools = {}
 local conf = require('modules.tools.config')
 
-tools['kristijanhusak/vim-dadbod-ui'] = {
-  cmd = {'DBUIToggle','DBUIAddConnection','DBUI','DBUIFindBuffer','DBUIRenameBuffer'},
-  config = conf.vim_dadbod_ui,
-  requires = {{'tpope/vim-dadbod',opt = true}}
-}
-
 tools['editorconfig/editorconfig-vim'] = {
-  ft = { 'go','typescript','javascript','vim','c'}
+  ft = { 'go','typescript','javascript','vim','c','lua'}
 }
-
 
 tools['liuchengxu/vista.vim'] = {
   cmd = 'Vista',
   config = conf.vim_vista
-}
-
-tools['brooth/far.vim'] = {
-  cmd = {'Far','Farp'},
-  config = function ()
-    vim.g['far#source'] = 'rg'
-  end
 }
 
 return tools
